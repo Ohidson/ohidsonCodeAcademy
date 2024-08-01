b8fb4372365531332aa6f56526a97fe721264d16
Ohidson <ohidson@gmail.com>
2024-08-01 12:51:55 -0900

    changes commit

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ var x = 0
 
 // generating list
 images[0] = "asset/images/Ohidson1.jpg"
-images[1] = "asset/images/Aregbesola.jpeg"
-images[1] = "asset/images/Aregbesola.jpeg"
-images[2] = "asset/images/volleyball.jpg"
-images[3] = "asset/images/washington.jpg"
-images[4] = 'asset/images/libr.jpg'
-images[5] = "asset/images/f_student.jpg"
-images[6] = "asset/images/ohidson.JPG"
+images[1] = "asset/images/dinning3.jpg"
+images[2] = "asset/images/student.jpg"
+images[3] = "asset/images/volleyball.jpg"
+images[4] = "asset/images/washington.jpg"
+images[5] = 'asset/images/libr.jpg'
+images[6] = "asset/images/f_student.jpg"
+images[7] = "asset/images/codeSchool.jpg"
 
 //Definding our function
 const slidePics = () => {
